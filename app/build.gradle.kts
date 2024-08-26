eaa2cf7c1d4b804bc21f7fffbb98356813511534
cna <cna.mhmdi0435@gmail.com>
2024-08-26 23:45:12 +0330

    init commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+
+    id("com.vanniktech.maven.publish") version "0.29.0"
+}
+
+android {
+    namespace = "com.cna.loadingdots"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.cna.loadingdots"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ loadingdots/src/main/java/com/cna/loadingdots/LoadingDots.kt
@@ -0,0 +1,108 @@
+package com.cna.loadingdots
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class LoadingDots: View {
+
+    private var circlePaint = Paint()
+    private var circleRadius = 5f
+    private var targetRadius = 10f
+    private var animationDuration = 500L
+    private var numCircles = 3
+    private var circleColor = Color.WHITE
+    private val animators = mutableListOf<ValueAnimator>()
+    private val radiusList: MutableList<Float> by lazy { MutableList(numCircles) { circleRadius } }
+
+    constructor(context: Context) : super(context) {
+        init(null)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs)
+    }
+
+    private fun init(attrs: AttributeSet?) {
+        circlePaint.isAntiAlias = true
+        circlePaint.color = Color.WHITE
+        circlePaint.style = Paint.Style.FILL
+
+        attrs?.let {
+            val typedArray = context.obtainStyledAttributes(it, R.styleable.LoadingDots)
+            numCircles = typedArray.getInt(R.styleable.LoadingDots_numCircles, numCircles)
+            animationDuration = typedArray.getInt(R.styleable.LoadingDots_animationDuration, animationDuration.toInt()).toLong()
+            circleColor = typedArray.getColor(R.styleable.LoadingDots_circleColor, circleColor)
+            typedArray.recycle()
+        }
+
+        radiusList.clear()
+        radiusList.addAll(MutableList(numCircles) {circleRadius} )
+        animators.clear()
+
+        for (i in 0 until numCircles) {
+            val animator = createValueAnimator(i)
+            animators.add(animator)
+        }
+    }
+
+    private fun createValueAnimator(index: Int): ValueAnimator {
+        val animator = ValueAnimator.ofFloat(circleRadius, targetRadius, circleRadius)
+        animator.duration = animationDuration
+        animator.repeatMode = ValueAnimator.RESTART
+        animator.repeatCount = ValueAnimator.INFINITE
+        animator.startDelay = index * (animationDuration / numCircles)
+        animator.addUpdateListener { animation ->
+            val animatedValue = animation.animatedValue as Float
+            radiusList[index] = animatedValue
+
+            circlePaint.color = circleColor
+
+
+            postInvalidate()
+        }
+        animator.start()
+        return animator
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        for (i in 0 until numCircles) {
+            drawCircle(canvas, i)
+        }
+    }
+
+    private fun drawCircle(canvas: Canvas, index: Int) {
+        val x = (2 * index + 1) * targetRadius
+        val y = height / 2f
+        val radius = radiusList[index]
+        canvas.drawCircle(x, y, radius, circlePaint)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val desiredWidth = (2 * numCircles * targetRadius).toInt()
+        val desiredHeight = (2 * targetRadius).toInt()
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+
+        val finalWidth = when (widthMode) {
+            MeasureSpec.EXACTLY -> widthSize
+            MeasureSpec.AT_MOST -> desiredWidth.coerceAtMost(widthSize)
+            else -> desiredWidth
+        }
+
+        val finalHeight = when (heightMode) {
+            MeasureSpec.EXACTLY -> heightSize
+            MeasureSpec.AT_MOST -> desiredHeight.coerceAtMost(heightSize)
+            else -> desiredHeight
+        }
+
+        setMeasuredDimension(finalWidth, finalHeight)
+    }
+}
